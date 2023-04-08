d47e70c3cf3619340165358f34660b904a47c615
Benjamin Lorenzo <benjalorenzo96@hotmail.com>
2023-04-08 15:17:35 -0300

    Segunda Entrega de la Primera

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import NavBar from './components/NavBar.jsx';
 import './App.css';
+import ItemListContainer from './components/ItemListContainer.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <ItemListContainer message="Mirá qué Cuadros!" />
     </div>
   );
 }
 
 export default App;
+
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import CartWidget from './CartWidget';
+
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Mi sitio web</Navbar.Brand>
+      <Navbar.Brand href="#home">Cuadros</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Inicio</Nav.Link>
           <Nav.Link href="#link">Nosotros</Nav.Link>
           <NavDropdown title="Productos" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Producto 1</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Producto 2</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Producto 3</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Cuadros de Fútbol</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Cuadros de Camisetas</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Cuadro de Próceres Futbolísticos</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Promociones</NavDropdown.Item>
           </NavDropdown>
         </Nav>
+        <CartWidget />
       </Navbar.Collapse>
     </Navbar>
   );
 }
+
 
 export default NavBar;
